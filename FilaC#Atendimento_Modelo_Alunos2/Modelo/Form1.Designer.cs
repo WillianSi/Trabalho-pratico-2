05473a2fbf69e2136258638453cef8bf4df6dc5a
WillianSi <70384084+WillianSi@users.noreply.github.com>
2021-05-18 10:51:15 -0300

    Filas

---
+++ FilaC#Atendimento_Modelo_Alunos2/Modelo/Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿namespace Modelo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSobre = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.txtRG = new System.Windows.Forms.TextBox();
+            this.lblProx = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listPref = new System.Windows.Forms.ListBox();
+            this.listNormal = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnAtender = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Navy;
+            this.panel1.Controls.Add(this.btnSobre);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(141, 688);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnSobre
+            // 
+            this.btnSobre.FlatAppearance.BorderSize = 0;
+            this.btnSobre.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSobre.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSobre.ForeColor = System.Drawing.Color.White;
+            this.btnSobre.Location = new System.Drawing.Point(0, 0);
+            this.btnSobre.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnSobre.Name = "btnSobre";
+            this.btnSobre.Size = new System.Drawing.Size(141, 90);
+            this.btnSobre.TabIndex = 9;
+            this.btnSobre.Text = "Sobre";
+            this.btnSobre.UseVisualStyleBackColor = true;
+            this.btnSobre.Click += new System.EventHandler(this.BtnSobre_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Navy;
+            this.panel2.Controls.Add(this.btnSave);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.btnFechar);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(141, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1146, 154);
+            this.panel2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 21);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nome:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(106, 46);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(284, 29);
+            this.txtNome.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtIdade);
+            this.groupBox1.Controls.Add(this.txtRG);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtNome);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(220, 322);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(445, 313);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Adicionar Pessoa";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 21);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Idade:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 21);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "RG:";
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.Location = new System.Drawing.Point(106, 155);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(284, 29);
+            this.txtIdade.TabIndex = 7;
+            // 
+            // txtRG
+            // 
+            this.txtRG.Location = new System.Drawing.Point(106, 100);
+            this.txtRG.Name = "txtRG";
+            this.txtRG.Size = new System.Drawing.Size(284, 29);
+            this.txtRG.TabIndex = 6;
+            // 
+            // lblProx
+            // 
+            this.lblProx.AutoSize = true;
+            this.lblProx.BackColor = System.Drawing.Color.Red;
+            this.lblProx.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblProx.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProx.ForeColor = System.Drawing.Color.Yellow;
+            this.lblProx.Location = new System.Drawing.Point(476, 175);
+            this.lblProx.MinimumSize = new System.Drawing.Size(500, 100);
+            this.lblProx.Name = "lblProx";
+            this.lblProx.Size = new System.Drawing.Size(500, 100);
+            this.lblProx.TabIndex = 12;
+            this.lblProx.Text = "Prox";
+            this.lblProx.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Firebrick;
+            this.label1.Location = new System.Drawing.Point(713, 322);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 25);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Fila Preferencial";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label5.Location = new System.Drawing.Point(1006, 322);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 25);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Fila Normal";
+            // 
+            // listPref
+            // 
+            this.listPref.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listPref.FormattingEnabled = true;
+            this.listPref.ItemHeight = 21;
+            this.listPref.Location = new System.Drawing.Point(718, 354);
+            this.listPref.Name = "listPref";
+            this.listPref.Size = new System.Drawing.Size(241, 298);
+            this.listPref.TabIndex = 15;
+            // 
+            // listNormal
+            // 
+            this.listNormal.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listNormal.FormattingEnabled = true;
+            this.listNormal.ItemHeight = 21;
+            this.listNormal.Location = new System.Drawing.Point(1011, 354);
+            this.listNormal.Name = "listNormal";
+            this.listNormal.Size = new System.Drawing.Size(241, 298);
+            this.listNormal.TabIndex = 16;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.FlatAppearance.BorderSize = 0;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Image = global::Modelo.Properties.Resources.fracture;
+            this.btnAdd.Location = new System.Drawing.Point(134, 217);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(191, 81);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnAtender
+            // 
+            this.btnAtender.FlatAppearance.BorderSize = 0;
+            this.btnAtender.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAtender.Image = global::Modelo.Properties.Resources.doctor;
+            this.btnAtender.Location = new System.Drawing.Point(328, 190);
+            this.btnAtender.Name = "btnAtender";
+            this.btnAtender.Size = new System.Drawing.Size(142, 84);
+            this.btnAtender.TabIndex = 9;
+            this.btnAtender.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnAtender.UseVisualStyleBackColor = true;
+            this.btnAtender.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.FlatAppearance.BorderSize = 0;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Image = global::Modelo.Properties.Resources.save;
+            this.btnSave.Location = new System.Drawing.Point(1039, 105);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(95, 43);
+            this.btnSave.TabIndex = 15;
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Red;
+            this.pictureBox1.Location = new System.Drawing.Point(335, -1);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(536, 154);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.FlatAppearance.BorderSize = 0;
+            this.btnFechar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFechar.Image = global::Modelo.Properties.Resources.close;
+            this.btnFechar.Location = new System.Drawing.Point(1039, -1);
+            this.btnFechar.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(107, 68);
+            this.btnFechar.TabIndex = 12;
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.BtnFechar_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Modelo.Properties.Resources.images__1_;
+            this.pictureBox2.Location = new System.Drawing.Point(347, 15);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(508, 122);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 16;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1287, 688);
+            this.Controls.Add(this.listNormal);
+            this.Controls.Add(this.listPref);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblProx);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnAtender);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSobre;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnAtender;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtIdade;
+        private System.Windows.Forms.TextBox txtRG;
+        private System.Windows.Forms.Label lblProx;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ListBox listPref;
+        private System.Windows.Forms.ListBox listNormal;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ FilaC#Atendimento_Modelo_Alunos2/Modelo/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Modelo
+{
+    public partial class Form1 : Form
+    {
+        Queue<Pessoa> filaNormal = new Queue<Pessoa>();
+        Queue<Pessoa> filaPref = new Queue<Pessoa>();
+        int cont = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            carregar();
+            mostra();
+        }
+
+        private void BtnFechar_Click(object sender, EventArgs e)
+        {
+            
+            this.Close();
+        }
+
+        private void BtnSobre_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void salvar()
+        {
+            using (BinaryWriter writer = new BinaryWriter(File.Open("fila1.txt", FileMode.Create)))
+            {
+                writer.Write(filaNormal.Count());
+                foreach (Pessoa p in filaNormal)
+                {
+                    writer.Write(p.Nome);
+                    writer.Write(p.Rg);
+                    writer.Write(p.Idade);
+                }
+            }
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open("fila2.txt", FileMode.Create)))
+            {
+                writer.Write(filaPref.Count());
+                foreach (Pessoa p in filaPref)
+                {
+                    writer.Write(p.Nome);
+                    writer.Write(p.Rg);
+                    writer.Write(p.Idade);
+                }
+            }
+        }
+
+        void carregar()
+        {
+            if (File.Exists("fila1.txt"))
+            {
+                using (BinaryReader reader = new BinaryReader(File.Open("fila1.txt", FileMode.Open)))
+                {
+                    int qtd = reader.ReadInt32();
+                    for (int i = 0; i < qtd; i++)
+                    {
+                        Pessoa p = new Pessoa();
+                        p.Nome = reader.ReadString();
+                        p.Rg = reader.ReadInt32();
+                        p.Idade = reader.ReadInt32();
+                        filaNormal.Enqueue(p);
+                    }// fim for
+                }
+
+            }// Fila normal
+
+            if (File.Exists("fila2.txt"))
+            {
+                using (BinaryReader reader = new BinaryReader(File.Open("fila2.txt", FileMode.Open)))
+                {
+                    int qtd = reader.ReadInt32();
+                    for (int i = 0; i < qtd; i++)
+                    {
+                        Pessoa p = new Pessoa();
+                        p.Nome = reader.ReadString();
+                        p.Rg = reader.ReadInt32();
+                        p.Idade = reader.ReadInt32();
+                        filaPref.Enqueue(p);
+                    }// fim for
+                }
+
+            }// Fila normal
+
+            //mostra();
+        }
+
+        void mostra()
+        {
+            listNormal.Items.Clear();
+            foreach(Pessoa s in filaNormal)
+            {
+                listNormal.Items.Add(s.Nome + " - Idade:" +  s.Idade);
+            }
+
+            listPref.Items.Clear();
+            foreach (Pessoa s in filaPref)
+            {
+                listPref.Items.Add(s.Nome + " - Idade:" + s.Idade);
+            }
+        }
+        //---------------
+        void limpa()
+        {
+            txtNome.Clear();
+            txtIdade.Clear();
+            txtRG.Clear();
+            txtNome.Focus();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Pessoa p = new Pessoa();
+
+            p.Nome = txtNome.Text;
+            p.Idade = Convert.ToInt32(txtIdade.Text);
+            p.Rg = Convert.ToInt32(txtRG.Text);
+            
+            if(p.Idade >= 60)
+            {
+                filaPref.Enqueue(p);
+            }
+            else
+            {
+                filaNormal.Enqueue(p);
+            }
+
+            mostra();
+            limpa();
+        }
+
+        void removePref()
+        {
+            filaPref.Dequeue();
+        }
+
+        void removeNormal()
+        {
+            filaNormal.Dequeue();  
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+
+            while(filaPref.Count >= 0 && filaPref.Count >= 0)
+            {
+                Pessoa p = new Pessoa();
+                Pessoa n = new Pessoa();
+
+                if (cont < 3 && filaPref.Count > 0)
+                {
+                    p = filaPref.Peek();
+                    removePref();
+                    lblProx.Text = p.Nome;
+                    mostra();
+                    cont++;
+                    break;
+                }
+                
+                else
+                { 
+                    n = filaNormal.Peek();
+                    removeNormal();
+                    lblProx.Text = n.Nome;
+                    mostra();
+                    cont = 0;
+                    break;               
+                }
+            }
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            salvar();
+        }
+    }
+}
